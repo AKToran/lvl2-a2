d6e5be6ae98eed25a0803e5c534aec3bad0c18b1
AKToran <aktoran60@gmail.com>
2026-07-09 10:49:22 +0600

    database tables created

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,8 +8,30 @@ const pool = new Pool({
 export const initDB = async () =>{
   try {
     await pool.query(`
-      
-      `)
+      CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(60) NOT NULL,
+      email VARCHAR(50) UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+    `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS issues (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(150) NOT NULL,
+      description TEXT NOT NULL CHECK(LENGTH(description) >= 20),
+      type VARCHAR(20) NOT NULL CHECK(type in ('bug', 'feature_request')),
+      status VARCHAR(15) DEFAULT 'open' CHECK(status in ('open', 'in_progress', 'resolved')),
+      reporter_id INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )  
+    `)
 
     console.log("Connected to Database!");
   } catch (error) {
